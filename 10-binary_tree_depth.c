900956a1fbf0f7945a07bead8392dabf319da95e
Alatise Oluwaseun <holynation667@gmail.com>
2022-08-25 23:39:49 +0100

    feat: Added task-10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - calculate levels from parent
+ * 
+ * Description: This is a function that measures the depth
+ * of a node in a binary tree
+ * 
+ * @node: node
+ * Return: levels node is from parent
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node && (node->parent))
+		return (1 + binary_tree_depth(node->parent));
+
+	return (0);
+}
